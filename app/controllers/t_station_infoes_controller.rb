36630d25d9f60cec72eb3fb7e68ab892f826a3ee
Your Name <you@example.com>
2017-09-06 10:29:18 +0800

    沧州参考人数示意图和考试情况示意图，测试专用

--- app/controllers/t_station_infoes_controller.rb
+++ app/controllers/t_station_infoes_controller.rb
@@ -1,28 +1,32 @@
 class TStationInfoesController < ApplicationController
+    def index
+        @stations = TStationInfo.all.where(F_duan_uuid: params[:duan])
+    end
 
+    def station_student_info
+        @duan = TDuanInfo.find_by(F_name: params[:name])
+        @station_student = TUserInfo.distinct(:F_id).joins(:t_station_info).where('t_station_info.F_duan_uuid = ?', TDuanInfo.find_by(F_name: params[:name]).F_uuid).group('t_station_info.F_name').size
+        gon.key = @station_student.keys
+        gon.value = @station_student.values
+        @station_student_ck = TUserInfo.joins(:t_station_info, :t_record_infoes).where('t_station_info.F_duan_uuid = ?', TDuanInfo.find_by(F_name: params[:name]).F_uuid).select('t_user_info.F_id,t_station_info.F_name').distinct(:F_id).group('t_station_info.F_name').size
+        gon.ckvalue = @station_student_ck.values
+    end
 
-  def index
-    @stations = TStationInfo.all.where(:F_duan_uuid => params[:duan])
-  end
+    def station_score_info
+        @station_90_scores = TStationInfo.where('t_station_info.F_duan_uuid = ?', TDuanInfo.find_by(F_name: params[:name]).F_uuid).joins(t_user_infoes: :t_record_infoes).where('t_record_info.F_score >= ?', 90).group('t_station_info.F_name').size
+        @station_80_scores = TStationInfo.where('t_station_info.F_duan_uuid = ?', TDuanInfo.find_by(F_name: params[:name]).F_uuid).joins(t_user_infoes: :t_record_infoes).where('t_record_info.F_score >= ? and t_record_info.F_score < ?', 80, 90).group('t_station_info.F_name').size
+        @station_60_scores = TStationInfo.where('t_station_info.F_duan_uuid = ?', TDuanInfo.find_by(F_name: params[:name]).F_uuid).joins(t_user_infoes: :t_record_infoes).where('t_record_info.F_score >= ? and t_record_info.F_score < ?', 60, 80).group('t_station_info.F_name').size
+        @station_60_bellow_scores = TStationInfo.where('t_station_info.F_duan_uuid = ?', TDuanInfo.find_by(F_name: params[:name]).F_uuid).joins(t_user_infoes: :t_record_infoes).where('t_record_info.F_score < ?', 60).group('t_station_info.F_name').size
+        gon.station_key = @station_90_scores.keys
+        gon.ninefen = @station_90_scores.values
+        gon.ef = @station_80_scores.values
+        gon.sf = @station_60_scores.values
+        gon.sb = @station_60_bellow_scores.values
+    end
 
-  def station_student_info
-    @duan = TDuanInfo.find_by(:F_name => params[:name])
-    @station_student = TUserInfo.distinct(:F_id).joins(:t_station_info).where("t_station_info.F_duan_uuid = ?", TDuanInfo.find_by(:F_name => params[:name]).F_uuid).group("t_station_info.F_name").size
-    @station_student_ck = TUserInfo.joins(:t_station_info,:t_record_infoes).where("t_station_info.F_duan_uuid = ?", TDuanInfo.find_by(:F_name => params[:name]).F_uuid).select("t_user_info.F_id,t_station_info.F_name").distinct(:F_id).group("t_station_info.F_name").size
-  end
+    private
 
-  def station_score_info
-    @station_90_scores = TStationInfo.where("t_station_info.F_duan_uuid = ?", TDuanInfo.find_by(:F_name => params[:name]).F_uuid).joins(t_user_infoes: :t_record_infoes).where("t_record_info.F_score >= ?",90).group("t_station_info.F_name").size
-    @station_80_scores = TStationInfo.where("t_station_info.F_duan_uuid = ?", TDuanInfo.find_by(:F_name => params[:name]).F_uuid).joins(t_user_infoes: :t_record_infoes).where("t_record_info.F_score >= ? and t_record_info.F_score < ?",80,90).group("t_station_info.F_name").size
-    @station_60_scores = TStationInfo.where("t_station_info.F_duan_uuid = ?", TDuanInfo.find_by(:F_name => params[:name]).F_uuid).joins(t_user_infoes: :t_record_infoes).where("t_record_info.F_score >= ? and t_record_info.F_score < ?",60,80).group("t_station_info.F_name").size
-    @station_60_bellow_scores = TStationInfo.where("t_station_info.F_duan_uuid = ?", TDuanInfo.find_by(:F_name => params[:name]).F_uuid).joins(t_user_infoes: :t_record_infoes).where("t_record_info.F_score < ?",60).group("t_station_info.F_name").size
-  end
-
-
-  private
-
-  def t_station_info_params
-    params.require(:t_station_info).permit(:F_name, :F_duan_uuid, :F_level, :Level)
-  end
-
+    def t_station_info_params
+        params.require(:t_station_info).permit(:F_name, :F_duan_uuid, :F_level, :Level)
+    end
 end
