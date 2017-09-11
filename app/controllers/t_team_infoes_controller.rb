c599e89f83f15cba08ad44904ff192ac08df7232
leichaofan <leichaofan2024@gmail.com>
2017-09-11 16:00:09 +0800

    complete navigation of every page

--- app/controllers/t_team_infoes_controller.rb
+++ app/controllers/t_team_infoes_controller.rb
@@ -6,6 +6,7 @@ class TTeamInfoesController < ApplicationController
     end
 
     def team_student_info
+      @duan = TDuanInfo.find_by(F_name: params[:duan_name])
         @station = TStationInfo.find_by(F_name: params[:name])
         @team_student = TUserInfo.student_all.joins(:t_team_info).where('t_team_info.F_station_uuid = ?', TStationInfo.find_by(F_name: params[:name]).F_uuid).select('t_user_info.F_id,t_team_info.F_name').distinct.group('t_team_info.F_name').size
         gon.key = @team_student.keys
@@ -15,6 +16,8 @@ class TTeamInfoesController < ApplicationController
     end
 
     def team_score_info
+        @duan = TDuanInfo.find_by(F_name: params[:duan_name])
+        @station = TStationInfo.find_by(F_name: params[:name])
         @team_90_scores = TTeamInfo.where('t_team_info.F_station_uuid = ?', TStationInfo.find_by(F_name: params[:name]).F_uuid).joins(t_user_infoes: :t_record_infoes).where('t_record_info.F_score >= ?', 90).group('t_team_info.F_name').size
         @team_80_scores = TTeamInfo.where('t_team_info.F_station_uuid = ?', TStationInfo.find_by(F_name: params[:name]).F_uuid).joins(t_user_infoes: :t_record_infoes).where('t_record_info.F_score >= ? and t_record_info.F_score < ?', 80, 90).group('t_team_info.F_name').size
         @team_60_scores = TTeamInfo.where('t_team_info.F_station_uuid = ?', TStationInfo.find_by(F_name: params[:name]).F_uuid).joins(t_user_infoes: :t_record_infoes).where('t_record_info.F_score >= ? and t_record_info.F_score < ?', 60, 80).group('t_team_info.F_name').size
