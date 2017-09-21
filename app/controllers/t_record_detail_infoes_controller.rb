1e4782642e42f6d9eab5de4ceac4e05a023de99f
Your Name <you@example.com>
2017-09-21 09:49:16 +0800

    未解决link_to的历史问题

--- app/controllers/t_record_detail_infoes_controller.rb
+++ app/controllers/t_record_detail_infoes_controller.rb
@@ -1,13 +1,12 @@
 class TRecordDetailInfoesController < ApplicationController
-  def record_details
-    @duan = TDuanInfo.find_by(F_name: params[:duan_name])
-    @station =TStationInfo.find_by(F_name: params[:name])
-    @team = TTeamInfo.where(:F_station_uuid => @station.F_uuid).find_by(F_name: params[:team_name])
-    @users = TUserInfo.where(:F_id => params[:user_id],:F_name => params[:user_name])
-    @record = TRecordInfo.find_by(:F_uuid => params[:record_uuid])
+    def record_details
+        @duan = TDuanInfo.find_by(F_name: params[:duan_name])
+        @station = TStationInfo.find_by(F_name: params[:station_name])
+        @team = TTeamInfo.where(F_station_uuid: @station.F_uuid).find_by(F_name: params[:team_name])
+        @users = TUserInfo.where(F_id: params[:user_id], F_name: params[:user_name])
+        @record = TRecordInfo.find_by(F_uuid: params[:record_uuid])
 
-
-    @t_record_detail_infoes =@record.t_record_detail_infoes
-    @teacher = TUserInfo.find_by(:F_uuid => @record.F_teacher_uuid)
-  end
+        @t_record_detail_infoes = @record.t_record_detail_infoes
+        @teacher = TUserInfo.find_by(F_uuid: @record.F_teacher_uuid)
+    end
 end
--- app/controllers/t_record_infoes_controller.rb
+++ app/controllers/t_record_infoes_controller.rb
@@ -1,28 +1,23 @@
 class TRecordInfoesController < ApplicationController
-  def index
-    @duan = TDuanInfo.find_by(F_name: params[:duan])
-    @station =TStationInfo.find_by(F_name: params[:station])
-    @team = TTeamInfo.where(:F_station_uuid => @station.F_uuid).find_by(F_name: params[:team])
-    @user = TUserInfo.find_by(F_id: params[:F_id])
-    @records = TRecordInfo.joins(:t_user_info).where("t_user_info.F_id = ?",params[:F_id])
-  end
+    def index
+        @duan = TDuanInfo.find_by(F_name: params[:duan_name])
+        @station = TStationInfo.find_by(F_name: params[:station_name])
+        @team = TTeamInfo.where(F_station_uuid: @station.F_uuid).find_by(F_name: params[:team_name])
+        @user = TUserInfo.find_by(F_id: params[:F_id])
+        @records = TRecordInfo.joins(:t_user_info).where('t_user_info.F_id = ?', params[:F_id])
+    end
 
-  def student_records
-    @duan = TDuanInfo.find_by(F_name: params[:duan_name])
-    @station =TStationInfo.find_by(F_name: params[:name])
-    @team = TTeamInfo.where(:F_station_uuid => @station.F_uuid).find_by(F_name: params[:team_name])
-    students = TUserInfo.where(:F_name => params[:user_name],:F_id => params[:user_id])
-    @records = TRecordInfo.where(:F_user_uuid => students.ids)
+    def student_records
+        @duan = TDuanInfo.find_by(F_name: params[:duan_name])
+        @station = TStationInfo.find_by(F_name: params[:station_name])
+        @team = TTeamInfo.where(F_station_uuid: @station.F_uuid).find_by(F_name: params[:team_name])
+        students = TUserInfo.where(F_name: params[:user_name], F_id: params[:user_id])
+        @records = TRecordInfo.where(F_user_uuid: students.ids)
+    end
 
-  end
-
-  def destroy
-    @record = TRecordInfo.find(params[:id])
-    @record.destroy
-    redirect_to team_student_info_t_team_infoes_path(:duan_name => params[:duan_name],:name => params[:name],:team_name=> params[:team_name])
-  end
-
-
-
-
+    def destroy
+        @record = TRecordInfo.find(params[:id])
+        @record.destroy
+        redirect_to team_student_info_t_team_infoes_path(duan_name: params[:duan_name], station_name: params[:station_name], team_name: params[:team_name])
+    end
 end
