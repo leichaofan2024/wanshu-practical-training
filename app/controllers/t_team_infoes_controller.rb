7ab8cfb61e8c9a1a5cf82b0e131669d241b1eb67
leichaofan <leichaofan2024@gmail.com>
2017-10-15 14:59:34 +0800

    fix bug of team-page

--- app/controllers/t_team_infoes_controller.rb
+++ app/controllers/t_team_infoes_controller.rb
@@ -16,12 +16,12 @@ class TTeamInfoesController < ApplicationController
             @search = TimeSearch.new(params[:search])
           if params[:team_name].present?
             @team = TTeamInfo.where(F_station_uuid: @station.F_uuid).find_by(F_name: params[:team_name])
-            @student_ck = @search.scope_team_student2(@team).select(:F_name, :F_id).distinct
-            @student_wk = TUserInfo.student_all.joins(:t_team_info).where('t_team_info.F_uuid=?', @team.F_uuid).select(:F_name, :F_id).distinct.where.not(F_id: @student_ck.pluck(:F_id))
+            @student_ck = @search.scope_team_student2(@team).select(:F_name, :F_id,:F_work_uuid,:F_team_uuid).distinct
+            @student_wk = TUserInfo.student_all.joins(:t_team_info).where('t_team_info.F_uuid=?', @team.F_uuid).select(:F_name, :F_id,:F_work_uuid,:F_team_uuid).distinct.where.not(F_id: @student_ck.pluck(:F_id))
 
           else
-            @student_ck = @search.scope_team_student3(@station).select(:F_name, :F_id).distinct
-            @student_wk = TUserInfo.student_all.joins(:t_station_info).where('t_station_info.F_uuid': @station.F_uuid).select(:F_name, :F_id).distinct.where.not(F_id: @student_ck.pluck(:F_id))
+            @student_ck = @search.scope_team_student3(@station).select(:F_name, :F_id,:F_work_uuid,:F_team_uuid).distinct
+            @student_wk = TUserInfo.student_all.joins(:t_station_info).where('t_station_info.F_uuid': @station.F_uuid).select(:F_name, :F_id,:F_work_uuid,:F_team_uuid).distinct.where.not(F_id: @student_ck.pluck(:F_id))
           end
             m = @search.scope_team_student(params[:station_name]).select('t_user_info.F_name,t_user_info.F_id,t_team_info.F_name').distinct
             c = m.group('t_team_info.F_name').count
@@ -48,11 +48,11 @@ class TTeamInfoesController < ApplicationController
         else
           if params[:team_name].present?
             @team = TTeamInfo.where(F_station_uuid: @station.F_uuid).find_by(F_name: params[:team_name])
-            @student_ck = TUserInfo.student_all.joins(:t_team_info, :t_record_infoes).where('t_team_info.F_uuid =? ', @team.F_uuid).select(:F_name, :F_id).distinct
-            @student_wk = TUserInfo.student_all.joins(:t_team_info).where('t_team_info.F_uuid=?', @team.F_uuid).select(:F_name, :F_id).distinct.where.not(F_id: @student_ck.pluck(:F_id))
+            @student_ck = TUserInfo.student_all.joins(:t_team_info, :t_record_infoes).where('t_team_info.F_uuid =? ', @team.F_uuid).select(:F_name, :F_id,:F_work_uuid,:F_team_uuid).distinct
+            @student_wk = TUserInfo.student_all.joins(:t_team_info).where('t_team_info.F_uuid=?', @team.F_uuid).select(:F_name, :F_id,:F_work_uuid,:F_team_uuid).distinct.where.not(F_id: @student_ck.pluck(:F_id))
           else
-            @student_ck = TUserInfo.student_all.joins(:t_station_info, :t_record_infoes).where('t_station_info.F_uuid ', @station.F_uuid).select(:F_name, :F_id).distinct
-            @student_wk = TUserInfo.student_all.joins(:t_station_info).where('t_station_info.F_uuid': @station.F_uuid).select(:F_name, :F_id).distinct.where.not(F_id: @student_ck.pluck(:F_id))
+            @student_ck = TUserInfo.student_all.joins(:t_station_info, :t_record_infoes).where('t_station_info.F_uuid': @station.F_uuid).select(:F_name, :F_id,:F_work_uuid,:F_team_uuid).distinct
+            @student_wk = TUserInfo.student_all.joins(:t_station_info).where('t_station_info.F_uuid': @station.F_uuid).select(:F_name, :F_id,:F_work_uuid,:F_team_uuid).distinct.where.not(F_id: @student_ck.pluck(:F_id))
           end
             m = TTeamInfo.where('t_team_info.F_station_uuid = ?', @station.F_uuid).joins(t_user_infoes: :t_record_infoes).where("t_user_info.F_type= ?", 0).select('t_user_info.F_name,t_user_info.F_id,t_team_info.F_name').distinct
             c = m.group('t_team_info.F_name').count
