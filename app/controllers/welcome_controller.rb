cb25630171802c79574dbffb2d382e3406dd6aed
leichaofan <leichaofan2024@gmail.com>
2017-10-16 02:14:53 +0800

    完成车站权限第三个卡片

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -116,6 +116,13 @@ class WelcomeController < ApplicationController
         @ck_programs = ck_programs.pluck("t_program_info.F_name")
         @wk_programs = programs.where.not(:F_id => ck_programs.ids).pluck("t_program_info.F_name")
       end
+    elsif current_user.permission == 3
+      if params[:program_type].present?
+        programs = TProgramInfo.where("t_program_info.F_type_id": TProgramTypeInfo.find_by(:F_name => params[:program_type]).F_id)
+        ck_programs = TProgramInfo.joins({t_record_detail_infoes: {t_record_info: :t_station_info}},:t_program_type_info).where("t_station_info.F_name= ?", current_user.orgnize).where("t_program_type_info.F_name": params[:program_type]).select(:F_name,:F_type_id).distinct
+        @ck_programs = ck_programs.pluck("t_program_info.F_name")
+        @wk_programs = programs.where.not(:F_id => ck_programs.ids).pluck("t_program_info.F_name")
+      end
     end
   end
 
