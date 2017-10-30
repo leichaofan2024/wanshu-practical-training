b1dd5cabbfadc6133d16a7979d97dcd1ddd89037
13949836783@163.com <13949836783@163.com>
2017-10-30 17:13:20 +0800

    可以编辑和删除角色

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:alert] = "已删除该角色"
+    redirect_to users_path
+  end
+
   private
 
   def user_params
