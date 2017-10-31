1cdaa89cd5be072f1e883b5ca3df4af2e9a834de
leichaofan <leichaofan2024@gmail.com>
2017-10-31 12:50:16 +0800

    change file of uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -10,10 +10,12 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
+  # def store_dir
+  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # end
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+     'uploads/image'
   end
-
   version :huge do
     process resize_to_fill: [1000,800]
   end
