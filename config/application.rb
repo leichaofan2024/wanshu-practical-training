0d6693d73f5256751fab4757d47181b337de7ac9
Your Name <you@example.com>
2017-11-23 16:37:43 +0800

    上传文件方法测试

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 require 'carrierwave'
 require 'carrierwave/orm/activerecord'
+require 'carrierwave/storage/ftp'
 
 require 'rails/all'
 
@@ -14,5 +15,15 @@ module WanshuPracticalTraining
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    CarrierWave.configure do |config|
+      config.ftp_host = "192.168.1.13"
+      config.ftp_port = 22
+      config.ftp_user = "wanshukeji"
+      config.ftp_passwd = "wanshukeji"
+      config.ftp_folder = "/E:/uploads"
+      config.ftp_url = "http://192.168.174.130:3000/uploads"
+      config.ftp_passive = false # false by default
+      config.ftp_tls = false # false by default
+    end
   end
 end
