f4146f8c4adc4a108632af9aeb125cb161b325a4
leichaofan <leichaofan2024@gmail.com>
2018-02-01 09:23:37 +0800

    修改时区

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -15,7 +15,7 @@ class ApplicationRecord < ActiveRecord::Base
   scope :program_record, -> (name) {where("t_record_info.F_uuid": TRecordDetailInfo.where("t_record_detail_info.F_program_id": TProgramInfo.program(name).F_id).pluck(:F_record_uuid))}
   scope :program_detail_reason, -> (name) {where("t_detail_reason_info.F_record_detail_uuid": TRecordDetailInfo.where("t_record_detail_info.F_program_id": TProgramInfo.program(name).F_id).pluck(:F_uuid))}
   scope :datetime, -> {where('t_record_info.F_time BETWEEN ? AND ?', Time.now.beginning_of_month, Time.now.end_of_month)}
-  scope :datetime1, -> {where('t_detail_reason_info.F_time BETWEEN ? AND ?', Time.now.beginning_of_month, Time.now.end_of_month)}
+  scope :datetime1, -> {where('t_detail_reason_info.F_time BETWEEN ? AND ?', Time.now.beginning_of_month+8.hours, Time.now.end_of_month+8.hours)}
 
 
 end
