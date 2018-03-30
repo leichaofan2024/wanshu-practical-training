46a10ac4d55c45ad0520d215c8d2add1f20a44e3
雷超凡 <leichaofan1987@163.com>
2018-03-30 15:30:35 +0800

    显示时长

--- app/helpers/t_team_infoes_helper.rb
+++ app/helpers/t_team_infoes_helper.rb
@@ -1,3 +1,18 @@
 module TTeamInfoesHelper
-  
+  def render_student_time_length(params,search,station)
+    s = TStationInfo.find_by(:F_name => station)
+    if search.present?
+      t = TimeSearch.new(search)
+
+      a = TRecordInfo.where("t_record_info.F_time between ? and ?",t.date_from,t.date_to).where(F_user_uuid: TUserInfo.where(:F_id => params.F_id,:F_station_uuid => s.F_uuid).pluck(:F_uuid)).sum("t_record_info.time_length")
+      b = a%60
+      c = (a - b)/60
+      return "#{c}分#{b}秒"
+    else
+      a = TRecordInfo.where("t_record_info.F_time between ? and ?",Time.now.beginning_of_month,Time.now.end_of_month).where(F_user_uuid: TUserInfo.where(:F_id => params.F_id,:F_station_uuid => s.F_uuid).pluck(:F_uuid)).sum("t_record_info.time_length")
+      b = a%60
+      c = (a - b)/60
+      return "#{c}分#{b}秒"
+    end
+  end
 end
