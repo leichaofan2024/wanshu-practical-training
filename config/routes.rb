6374248bcbd051964d085eded7d4d30816cd6f64
Your Name <you@example.com>
2018-05-17 11:21:35 +0800

    增加了详情页，在xcf的index中

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ Rails.application.routes.draw do
     namespace :xcf do
       get "/general_overview", to: "data_display#general_overview"
       get "/all_examinee", to: "data_display#all_examinee"
+      get "/index", to: "data_display#index"
     end
     resources :faqs
     resources :t_baogao_inputs
