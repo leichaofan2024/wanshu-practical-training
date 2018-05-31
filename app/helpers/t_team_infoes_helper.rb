bfce5a2265701cb352b6d1f2dcebbf964278d951
雷超凡 <leichaofan1987@163.com>
2018-05-31 23:46:34 +0800

    修改车站参考页面时长helper

--- app/helpers/t_team_infoes_helper.rb
+++ app/helpers/t_team_infoes_helper.rb
@@ -9,7 +9,7 @@ module TTeamInfoesHelper
       c = (a - b)/60
       return "#{c}分#{b}秒"
     else
-      a = TRecordInfo.where("t_record_info.F_time between ? and ?",Time.now.beginning_of_month,Time.now.end_of_month).where(F_user_uuid: TUserInfo.where(:F_id => params.F_id,:F_station_uuid => s.F_uuid).pluck(:F_uuid)).sum("t_record_info.time_length")
+      a = TRecordInfo.where("t_record_info.F_time between ? and ?",Time.now.beginning_of_month+8.hours,Time.now.end_of_month+8.hours).where(F_user_uuid: TUserInfo.where(:F_id => params.F_id,:F_station_uuid => s.F_uuid).pluck(:F_uuid)).sum("t_record_info.time_length")
       b = a%60
       c = (a - b)/60
       return "#{c}分#{b}秒"
