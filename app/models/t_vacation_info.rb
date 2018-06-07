a0f750cb972927ee6439170651876b13f5ea6a9d
雷超凡 <leichaofan1987@163.com>
2018-06-07 18:15:09 +0800

    修复车站卡片bug

--- app/models/t_vacation_info.rb
+++ app/models/t_vacation_info.rb
@@ -18,19 +18,24 @@ class TVacationInfo < ApplicationRecord
     a = TVacationInfo.all.where("t_vacation_infos.F_type = ? and t_vacation_infos.begin_time between ? and ?","调离",begin_time, end_time).select(:station_name,:F_id).distinct
       x = []
       a.each do |aa|
-        x += TUserInfo.where(:F_station_uuid => TStationInfo.find_by(:F_name => aa.station_name).F_uuid ,:F_id => aa.F_id).pluck(:F_uuid)
+        if TStationInfo.find_by(:F_name => aa.station_name).present?
+          x += TUserInfo.where(:F_station_uuid => TStationInfo.find_by(:F_name => aa.station_name).F_uuid ,:F_id => aa.F_id).pluck(:F_uuid)
+        end
       end
     b = TVacationInfo.all.where("t_vacation_infos.F_type = ? and t_vacation_infos.begin_time < ? and t_vacation_infos.end_time > ?","调离",begin_time, end_time).select(:station_name,:F_id).distinct
       y = []
       b.each do |bb|
-        y += TUserInfo.where(:F_station_uuid => TStationInfo.find_by(:F_name => bb.station_name).F_uuid ,:F_id => bb.F_id).pluck(:F_uuid)
+        if TStationInfo.find_by(:F_name => bb.station_name).present?
+          y += TUserInfo.where(:F_station_uuid => TStationInfo.find_by(:F_name => bb.station_name).F_uuid ,:F_id => bb.F_id).pluck(:F_uuid)
+        end
       end
     c = TVacationInfo.where("t_vacation_infos.end_time": nil).where("t_vacation_infos.F_type = ? and t_vacation_infos.begin_time < ?", "调离", begin_time).select(:station_name,:F_id).distinct
       z = []
       c.each do |cc|
-        z += TUserInfo.where(:F_station_uuid => TStationInfo.find_by(:F_name => cc.station_name).F_uuid ,:F_id => cc.F_id).pluck(:F_uuid)
+        if TStationInfo.find_by(:F_name => cc.station_name).present?
+          z += TUserInfo.where(:F_station_uuid => TStationInfo.find_by(:F_name => cc.station_name).F_uuid ,:F_id => cc.F_id).pluck(:F_uuid)
+        end
       end
-
     return x + y + z
   end
 
